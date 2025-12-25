980619c3d42419e107a69715e30e37be4838163d
hossam <hosam123138@gmail.com>
2025-12-25 10:04:57 +0300

    Update angular budget and server routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,20 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'products/:id',
+    renderMode: RenderMode.Server
+  },
+
+  {
+    path: 'blog/:id',
+    renderMode: RenderMode.Server
+  },
+
+
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender
   }
 ];
